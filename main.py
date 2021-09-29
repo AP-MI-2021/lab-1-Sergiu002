f77c4a5e8241a0300ad96141be548f4025803ac2
Sergiu002 <sergiu.eduard.suciu@stud.ubbcluj.ro>
2021-09-29 09:19:08 +0300

    Funtia is_prime

--- main.py
+++ main.py
@@ -37,15 +37,15 @@ def get_cmmdc_v1(x, y):
 Returneaza CMMDC a doua numere x si y folosind al doilea algoritm.
 '''
 def get_cmmdc_v2(x, y):
-  r = a % b
+  r = x % y
   while r != 0:
-    a = b
-    b = r
-    r = a % b
-  
+    x = y
+    y = r
+    r = x % y
+   return y
   
 def main():
-  # interfata de tip consola aici
+  
 
 if __name__ == '__main__':
   main()
