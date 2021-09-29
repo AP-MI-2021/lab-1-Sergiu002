b1f84e85350117da1caf56f4c24ac8b66ef14596
Sergiu002 <sergiu.eduard.suciu@stud.ubbcluj.ro>
2021-09-29 09:03:51 +0300

    Funtia is_prime

--- main.py
+++ main.py
@@ -2,28 +2,46 @@
 Returneaza true daca n este prim si false daca nu.
 '''
 def is_prime(n):
-  # codul vostru aici
-  
+  if n == 1:
+      return False
+  elif n == 2:
+    return True;
+  else:
+    for x in range(2, n):
+      if n % x == 0:
+        return False
+    return True
+
   
 '''
 Returneaza produsul numerelor din lista lst.
 '''
 def get_product(lst):
-  # codul vostru aici
+  result = 1
+  for x in lst:
+    result = result * x
+      return result
   
   
 '''
 Returneaza CMMDC a doua numere x si y folosind primul algoritm.
 '''
 def get_cmmdc_v1(x, y):
-  # codul vostru aici
+  if y == 0:
+    return x:
+  else:
+    return get_cmmdc_v1(y, (x % y))
   
   
 '''
 Returneaza CMMDC a doua numere x si y folosind al doilea algoritm.
 '''
 def get_cmmdc_v2(x, y):
-  # codul vostru aici
+  r = a % b
+  while r != 0:
+    a = b
+    b = r
+    r = a % b
   
   
 def main():
